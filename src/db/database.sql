ce2040ba4b4fc9f25c603e7f3b649e8e97c39b85
Caique <caiquenaimi.564@gmail.com>
2024-05-13 09:39:16 -0300

    feat: Update database schema
    This commit updates the database schema to reflect changes in the application's requirements. It adds new tables for artists, genres, albums, musics, playlists, and users. It also modifies the existing table name from "heroes" to "artists". This update allows for better organization and management of music-related data in the backend.
    
    Note: The commit message "git init: first commit" is not relevant to the current code changes and can be ignored.

--- src/db/database.sql
+++ src/db/database.sql
@@ -1,22 +1,55 @@
-CREATE DATABASE theboys;
+CREATE DATABASE backend_final;
 
-CREATE TABLE heroes (
+CREATE TABLE artists (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    image VARCHAR(100) NOT NULL,
-    skill VARCHAR(100) NOT NULL,
-    power INT NOT NULL,
-    level SMALLINT NOT NULL,
-    health INT NOT NULL,
-    winsCounter INT
+    name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE battles (
+CREATE TABLE genres (
     id SERIAL PRIMARY KEY,
-    hero1_id INT NOT NULL,
-    hero2_id INT NOT NULL,
-    winner_id INT NOT NULL,
-    loser_id INT NOT NULL,
-    FOREIGN KEY (hero1_id) REFERENCES heroes(id),
-    FOREIGN KEY (hero2_id) REFERENCES heroes(id)
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE albums (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    image VARCHAR(100),
+    artist_id INT NOT NULL,
+    release_year INT NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artists(id)
+);
+
+CREATE TABLE musics (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    image VARCHAR(100),
+    album_id INT NOT NULL,
+    duration TIME NOT NULL,
+    genre_id INT NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES albums(id),
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
+
+CREATE TABLE playlists (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE playlist_musics (
+    id SERIAL PRIMARY KEY,
+    playlist_id INT NOT NULL,
+    music_id INT NOT NULL,
+    position INT NOT NULL,
+    FOREIGN KEY (playlist_id) REFERENCES playlists(id),
+    FOREIGN KEY (music_id) REFERENCES musics(id)
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
 );
