78fb83b46d43564bc99ddf9dc0faf133fa34ca32
caurin <gabrielcaurin.564@gmail.com>
2024-05-15 08:12:06 -0300

    feat: remove genres

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const musicsRoutes = require('./routes/musicsRoutes');
-const usersRoutes = require('./routes/usersRoutes');
-const genresRoutes = require('./routes/genresRoutes');
-const artistsRoutes = require('./routes/artistsRoutes');
-const albumRoutes = require('./routes/albumRoutes');
-const playlistRoutes = require('./routes/playlistRoutes');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const musicsRoutes = require("./routes/musicsRoutes");
+const usersRoutes = require("./routes/usersRoutes");
+const artistsRoutes = require("./routes/artistsRoutes");
+const albumRoutes = require("./routes/albumRoutes");
+const playlistRoutes = require("./routes/playlistRoutes");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,13 +14,12 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use('/', musicsRoutes);
-app.use('/', usersRoutes);
-app.use('/', genresRoutes);
-app.use('/', artistsRoutes);
-app.use('/', albumRoutes);
-app.use('/', playlistRoutes);
+app.use("/", musicsRoutes);
+app.use("/", usersRoutes);
+app.use("/", artistsRoutes);
+app.use("/", albumRoutes);
+app.use("/", playlistRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running and routing on port ${port}`);
+  console.log(`Server is running and routing on port ${port}`);
 });
