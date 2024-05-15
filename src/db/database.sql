f1e9328190c0cdc71254b511cf189e4734f173b9
Caique <caiquenaimi.564@gmail.com>
2024-05-15 08:26:45 -0300

    feat: Remove album and artist controllers and routes
    This commit removes the albumController, artistsController, albumRoutes, and artistsRoutes files. These files are no longer needed as the functionality they provided has been refactored and integrated into other parts of the codebase. This change helps to simplify the code structure and improve maintainability.
    
    Note: The commit messages "Merge branch 'main' of https://github.com/caiquenaimi/Final-project-backend" and "git init: first commit" are not relevant to the current code changes and can be ignored.

--- src/db/database.sql
+++ src/db/database.sql
@@ -2,28 +2,14 @@ CREATE DATABASE backend_final;
 
 \c backend_final;
 
-CREATE TABLE artists (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
-
-
-CREATE TABLE albums (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    image VARCHAR(100),
-    artist_id INT NOT NULL,
-    release_year INT NOT NULL,
-    FOREIGN KEY (artist_id) REFERENCES artists(id)
-);
-
 CREATE TABLE musics (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     image VARCHAR(100),
-    album_id INT NOT NULL,
-    duration TIME NOT NULL,
-    FOREIGN KEY (album_id) REFERENCES albums(id),
+    duration DECIMAL(2,10) NOT NULL,
+    file VARCHAR(100) NOT NULL,
+    album VARCHAR(100) NOT NULL,
+    artist VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE users (
@@ -37,6 +23,7 @@ CREATE TABLE playlists (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description TEXT,
+    duration DECIMAL(2,10) NOT NULL,
     user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
@@ -44,6 +31,7 @@ CREATE TABLE playlists (
 CREATE TABLE members (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
     birthdate DATE NOT NULL,
-    description TEXT
+    image VARCHAR(100) 
 );
