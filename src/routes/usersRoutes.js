e2e0ea5a65a5b336a91f034b3e12350647296af8
Arthur Borges <arthurborges.senai564@gmail.com>
2024-05-29 08:02:32 -0300

    fix: Correct logout route in usersRoutes.js
    This commit fixes a typo in the logout route of the usersRoutes.js file. The route was previously defined as "/users/logout" but has been corrected to "/users/logOut" to match the corresponding function name in the usersController. This fix ensures that the logout functionality works as intended in the application.

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -7,7 +7,7 @@ router.get("/users", usersController.getAllUsers);
 router.get("/users/:name", usersController.getUserByName);
 router.post("/users/", usersController.createUser);
 router.post("/users/login", usersController.loginUser);
-router.post("/users/logout", usersController.logOut);
+router.post("/users/logOut", usersController.logOut);
 router.post("/users/refresh", usersController.refreshToken);
 router.put("/users/:id", usersController.updateUser);
 router.delete("/users/:id", usersController.deleteUser);
