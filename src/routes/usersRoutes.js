a3b2e4f77018ba6d651dc95e3cec8f2b3727aba1
Arthur Borges <arthurborges.senai564@gmail.com>
2024-05-29 10:31:42 -0300

    ```text feat: Add getUserById route in usersRoutes.js
    This commit adds a new route, "/users/:id", to the usersRoutes.js file. The route is responsible for retrieving a user's information based on their ID. This addition enhances the functionality of the application by allowing users to fetch specific user details.

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -9,6 +9,7 @@ router.post("/users", usersController.createUser);
 router.post("/users/login", usersController.loginUser);
 router.post("/users/logOut", usersController.logOut);
 router.post("/users/refresh", usersController.refreshToken);
+router.get("/users/:id", usersController.getUserById);
 router.put("/users/:id", usersController.updateUser);
 router.delete("/users/:id", usersController.deleteUser);
 
