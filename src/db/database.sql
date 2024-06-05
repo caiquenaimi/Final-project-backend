83cd82e37d2db9aa4bb242f74c198cacfc35bc1f
Gabriel Caurin <gabriel.caurin@aluno.senai.br>
2024-06-05 11:42:16 -0300

    feat: Remove birthdate and age fields from users table

--- src/db/database.sql
+++ src/db/database.sql
@@ -16,7 +16,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE rtoken (
